4ba3a4836860fc8d4f657f93875b544e16f26db9
Yasser Bigdeli <44984561+yasser64b@users.noreply.github.com>
2019-07-01 11:48:48 -0700

    Create EX1_pipInstall.py

---
+++ EX1_pipInstall.py
@@ -0,0 +1,9 @@
+import os 
+import sys
+PythonPath= os.path.dirname(sys.executable)
+print('\n')
+print('The python path is :', PythonPath)
+
+pipPath=PythonPath+'\Scripts' 
+print('Directory to scripts', pipPath)
+print('\n')
